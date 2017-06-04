e5ae735b01e2d246b98f87bfbbd1149363dcd44e
Mateusz P <bananq@gmail.com>
2017-06-04 19:13:57 +0200

    added migrations

---
+++ phinx/migrations/20170604113245_add_post_author_id.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddPostAuthorId extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $posts = $this->table('post');
+        $posts->addColumn('author_id', 'integer', ['null' => false]);
+        $posts->addForeignKey('author_id', 'author');
+        $posts->save();
+    }
+}
---
+++ src/templates/default/posts/post.php
@@ -0,0 +1,3 @@
+Author: <?php echo $author; ?> Created: <?php echo $post['created']; ?>
+<h1><?php echo $post['title']; ?></h1>
+<?php echo $post['content'];
